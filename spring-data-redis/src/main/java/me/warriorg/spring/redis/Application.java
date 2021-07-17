f94580832aa29f9a7e5bb60f419107504d1507e1
warriorg <warrior.gao@gmail.com>
2021-07-17 22:32:48 +0800

    study

--- spring-data-redis/src/main/java/me/warriorg/spring/redis/Application.java
+++ spring-data-redis/src/main/java/me/warriorg/spring/redis/Application.java
@@ -6,6 +6,8 @@ import me.warriorg.spring.redis.converter.BytesToMoneyConverter;
 import me.warriorg.spring.redis.converter.MoneyToBytesConverter;
 import me.warriorg.spring.redis.model.Coffee;
 import me.warriorg.spring.redis.service.CoffeeService;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,12 +16,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.data.redis.core.convert.RedisCustomConversions;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 @EnableTransactionManagement
@@ -27,9 +39,15 @@ import java.util.Optional;
 @EnableJpaRepositories
 @EnableRedisRepositories
 public class Application implements ApplicationRunner {
+    private static final String KEY = "COFFEE_MENU";
 
     @Autowired
     private CoffeeService coffeeService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private ReactiveStringRedisTemplate redisTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -40,14 +58,28 @@ public class Application implements ApplicationRunner {
         return builder -> builder.readFrom(ReadFrom.MASTER_PREFERRED);
     }
 
+
+
+
     @Bean
     public RedisCustomConversions redisCustomConversions() {
         return new RedisCustomConversions(
                 Arrays.asList(new MoneyToBytesConverter(), new BytesToMoneyConverter()));
     }
 
+    @Bean
+    ReactiveStringRedisTemplate reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        return new ReactiveStringRedisTemplate(factory);
+    }
+
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        reactive();
+    }
+
+    private void data() {
         Optional<Coffee> c = coffeeService.findSimpleCoffeeFromCache("mocha");
         log.info("Coffee {}", c);
 
@@ -56,5 +88,39 @@ public class Application implements ApplicationRunner {
         }
 
         log.info("Value from Redis: {}", c);
+    }
+
+    private void reactive() throws InterruptedException {
+        ReactiveHashOperations<String, String, String> hashOps = redisTemplate.opsForHash();
+        CountDownLatch cdl = new CountDownLatch(1);
+
+        List<Coffee> list = jdbcTemplate.query(
+                "select * from t_coffee", (rs, i) ->
+                        Coffee.builder()
+                                .id(rs.getLong("id"))
+                                .name(rs.getString("name"))
+                                .price(Money.of(CurrencyUnit.USD, rs.getLong("price")))
+                                .build()
+        );
+
+        Flux.fromIterable(list)
+                .publishOn(Schedulers.single())
+                .doOnComplete(() -> log.info("list ok"))
+                .flatMap(c -> {
+                    log.info("try to put {},{}", c.getName(), c.getPrice());
+                    return hashOps.put(KEY, c.getName(), c.getPrice().toString());
+                })
+                .doOnComplete(() -> log.info("set ok"))
+                .concatWith(redisTemplate.expire(KEY, Duration.ofMinutes(1)))
+                .doOnComplete(() -> log.info("expire ok"))
+                .onErrorResume(e -> {
+                    log.error("exception {}", e.getMessage());
+                    return Mono.just(false);
+                })
+                .subscribe(b -> log.info("Boolean: {}", b),
+                        e -> log.error("Exception {}", e.getMessage()),
+                        () -> cdl.countDown());
+        log.info("Waiting");
+        cdl.await();
     }
 }
