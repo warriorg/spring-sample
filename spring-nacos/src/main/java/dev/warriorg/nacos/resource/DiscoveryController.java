47592f6b6f443453467b7deafcc0e84a6209be8d
warriorg <warrior.gao@foxmail.com>
2022-02-03 16:55:50 +0800

    fix

--- spring-nacos/src/main/java/dev/warriorg/nacos/resource/DiscoveryController.java
+++ spring-nacos/src/main/java/dev/warriorg/nacos/resource/DiscoveryController.java
@@ -7,10 +7,7 @@ import com.alibaba.nacos.api.naming.listener.NamingEvent;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -42,9 +39,9 @@ public class DiscoveryController {
         });
     }
 
-    @RequestMapping(value = "/get", method = GET)
+    @RequestMapping(value = "/get/{serviceName}", method = GET)
     @ResponseBody
-    public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+    public List<Instance> get(@PathVariable String serviceName) throws NacosException {
         return namingService.getAllInstances(serviceName);
     }
 
